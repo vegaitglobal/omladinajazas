eb6961d56f6d7667813cb80c519a4021f7808052
Vladislav Milivojevic <v.milivojevic@vegaitsourcing.rs>
2019-11-21 15:31:19 +0100

    minor changes, console logs removed

--- wp-content/themes/skt-strong/js/watu/common.js
+++ wp-content/themes/skt-strong/js/watu/common.js
@@ -122,9 +122,6 @@ jQuery(document).ajaxComplete(function () {
       if (jQuery.inArray(questionIndex, Watu.question_ids) != -1) {
         var newText = i + ". " + jQuery(this).children().find("p").text().split(".")[1];
         i++;
-
-        // jQuery(this).find('.show-question-content').find('p').text(newText);
-
         if (jQuery(this).children().find("p").text().replace(/ /g, '').indexOf("[1]") >= 0) {
           jQuery(this).hide();
         } else {
@@ -132,7 +129,6 @@ jQuery(document).ajaxComplete(function () {
         var clonedObject = jQuery.extend({}, jQuery(this));
         questionDivs.push(clonedObject);
         }
-        // jQuery('#knowledge-test-achieved-points').append(jQuery(this).prop('outerHTML'));
         jQuery(this).hide();
       }
 
@@ -147,14 +143,12 @@ jQuery(document).ajaxComplete(function () {
             i++;
             this.children().first('p').text(newText);
             this.children().first('p').css("margin-bottom", "20px");
-            console.log(newText);
             this.show();
             jQuery('#qresults').append(this.prop('outerHTML'));
             return false;
           }
 
         });
-      //  console.log(this.children().find("p").text().split(".")[0].trim());
     });
     const unansweredQuestions = document.getElementsByClassName('unanswered');
     while (unansweredQuestions.length > 0) {
