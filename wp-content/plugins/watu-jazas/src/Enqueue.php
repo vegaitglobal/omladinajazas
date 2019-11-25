5334be19b5c154bf80a2cbe2f051f0b1843b5a74
Milos Roknic <m.roknic@vegaitsourcing.rs>
2019-11-25 14:54:07 +0100

    Remove dubug code

--- wp-content/plugins/watu-jazas/src/Enqueue.php
+++ wp-content/plugins/watu-jazas/src/Enqueue.php
@@ -4,9 +4,6 @@
 namespace WatuJazas;
 
 
-use ReflectionException;
-
-
 /**
  * Class Enqueue
  * @package WatuJazas\Base
@@ -25,9 +22,6 @@ class Enqueue extends PluginBase {
    * Registers all script and style files for the front
    */
   function enqueueFront() {
-
-    var_dump($this->getPluginUrl() . self::SCRIPTS_DIRECTORY . '/common.js');
-
     wp_enqueue_script('watu_jazas_front_script_common', $this->getPluginUrl() . self::SCRIPTS_DIRECTORY . '/common.js', ['jquery']);
     wp_enqueue_script('watu_jazas_front_script_knowledge_test', $this->getPluginUrl() . self::SCRIPTS_DIRECTORY . '/knowledge_test.js', ['jquery']);
     wp_enqueue_script('watu_jazas_front_script_risk_calculator', $this->getPluginUrl() . self::SCRIPTS_DIRECTORY . '/risk_calculator.js', ['jquery']);
